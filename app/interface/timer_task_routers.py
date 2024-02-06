1994e55643950ba719a41ea43f4cea16605fb1dd
Jax <c2hy@hotmail.com>
2024-02-06 23:13:13 +0800

    refactor: Adjust the object name

--- app/interface/timer_task_routers.py
+++ app/interface/timer_task_routers.py
@@ -5,19 +5,19 @@ from uuid import UUID
 from fastapi import APIRouter, Query
 from pydantic import BaseModel
 
-from app.domain.timers import DelayType, ComputationDelayType, DefinitionErrorType, State, DeadlineType, CycleType
+from app.domain.timer_tasks import DelayType, ComputationDelayType, DefinitionErrorType, State, DeadlineType, CycleType
 
 router = APIRouter()
 
 
-class TimerDefinition(BaseModel):
-    id: UUID
-    name: Optional[str]
-    note: Optional[str]
+class TimerTaskDefinition(BaseModel):
+    owner_id: str
+    union_code: Optional[str]
     group_id: Optional[UUID]
-    owner_id: Optional[str]
     notification_key: Optional[str]
     run_on_creation: bool = True
+    name: Optional[str]
+    note: Optional[str]
     delay_type: DelayType = DelayType.NONE
     fixed_delay_seconds: Optional[int]
     computation_delay_type: Optional[ComputationDelayType]
@@ -33,25 +33,25 @@ class TimerDefinition(BaseModel):
     deadline_on_ran_seconds: Optional[int]
 
 
-class TimerDefinitionResult(BaseModel):
-    id: Optional[UUID]
+class CreationTimerTaskResult(BaseModel):
+    task_id: Optional[UUID]
     is_ok: bool = True
     error_code: Optional[DefinitionErrorType]
 
 
 class TimerDescription(BaseModel):
-    id: Optional[UUID]
+    task_id: Optional[UUID]
     name: Optional[str]
     note: Optional[str]
 
 
 class ChangeableTimerState(BaseModel):
-    id: Optional[UUID]
+    task_id: Optional[UUID]
     state: State
 
 
-class TimerScheduleDefinition(BaseModel):
-    id: Optional[UUID]
+class TimerDefinition(BaseModel):
+    task_id: Optional[UUID]
     delay_type: DelayType = DelayType.NONE
     fixed_delay_seconds: Optional[int]
     computation_delay_type: Optional[ComputationDelayType]
@@ -67,17 +67,18 @@ class TimerScheduleDefinition(BaseModel):
     deadline_on_ran_seconds: Optional[int]
 
 
-class TimerDetails(BaseModel):
-    id: UUID
-    name: Optional[str]
-    lasted_run_datetime: Optional[datetime]
+class TimerTaskDetails(BaseModel):
+    task_id: UUID
     state: State
-    note: Optional[str]
+    lasted_run_datetime: Optional[datetime]
+    owner_id: str
+    union_code: Optional[str]
     group_id: Optional[UUID]
     group_name: Optional[str]
-    owner_id: Optional[str]
     notification_key: Optional[str]
     run_on_creation: bool
+    name: Optional[str]
+    note: Optional[str]
     delay_type: DelayType = DelayType.NONE
     fixed_delay_seconds: Optional[int]
     computation_delay_type: Optional[ComputationDelayType]
@@ -95,14 +96,16 @@ class TimerDetails(BaseModel):
     updated_at: datetime
 
 
-class Timer(BaseModel):
+class TimerTask(BaseModel):
     id: UUID
-    name: Optional[str]
-    lasted_run_datetime: Optional[datetime]
     state: State
-    note: Optional[str]
+    lasted_run_datetime: Optional[datetime]
+    owner_id: str
+    union_code: Optional[str]
     group_id: Optional[UUID]
     group_name: Optional[str]
+    name: Optional[str]
+    note: Optional[str]
     delay_type: DelayType = DelayType.NONE
     cycle_type: CycleType = CycleType.NONE
     deadline_type: DeadlineType = DeadlineType.NONE
@@ -110,66 +113,66 @@ class Timer(BaseModel):
     updated_at: datetime
 
 
-@router.post("/api/v1/timers")
-def create_timer(timer_definition: TimerDefinition) -> TimerDefinitionResult:
-    return TimerDefinitionResult()
+@router.post("/api/v1/timer-tasks")
+def create_timer_task(timer_definition: TimerTaskDefinition) -> CreationTimerTaskResult:
+    return CreationTimerTaskResult()
 
 
-@router.post("/api/v1/bulk-timers")
-def create_timers(timer_definitions: List[TimerDefinition]) -> TimerDefinitionResult:
-    return TimerDefinitionResult()
+@router.post("/api/v1/bulk-timer-tasks")
+def create_timer_tasks(timer_definitions: List[TimerTaskDefinition]) -> CreationTimerTaskResult:
+    return CreationTimerTaskResult()
 
 
-@router.delete("/api/v1/timers/{timer_id}")
-def delete_timer(timer_id: UUID) -> None:
+@router.delete("/api/v1/timer-tasks/{timer_id}")
+def delete_timer_task(timer_id: UUID) -> None:
     return None
 
 
-@router.delete("/api/v1/bulk-timers")
-def delete_timers(timer_ids: List[UUID]) -> None:
+@router.delete("/api/v1/bulk-timer-tasks")
+def delete_timer_tasks(timer_ids: List[UUID]) -> None:
     return None
 
 
-@router.put("/api/v1/timers/{timer_id}/description")
-def update_timer_description(timer_id: UUID, description: TimerDescription) -> None:
+@router.put("/api/v1/timer-tasks/{timer_id}/description")
+def update_timer_task_description(timer_id: UUID, description: TimerDescription) -> None:
     return None
 
 
-@router.put("/api/v1/bulk-timers/description")
-def batch_update_timer_description(descriptions: List[TimerDescription]) -> None:
+@router.put("/api/v1/bulk-timer-tasks/description")
+def batch_update_timer_task_description(descriptions: List[TimerDescription]) -> None:
     return None
 
 
-@router.put("/api/v1/timers/{timer_id}/state")
-def update_timer_state(timer_id: UUID, state: ChangeableTimerState) -> None:
+@router.put("/api/v1/timer-tasks/{timer_id}/state")
+def update_timer_task_state(timer_id: UUID, state: ChangeableTimerState) -> None:
     return None
 
 
-@router.put("/api/v1/bulk-timers/state")
-def batch_update_timer_state(states: ChangeableTimerState) -> None:
+@router.put("/api/v1/bulk-timer-tasks/state")
+def batch_update_timer_task_state(states: ChangeableTimerState) -> None:
     return None
 
 
-@router.put("/api/v1/timers/{timer_id}/schedule_definition")
-def update_timer_schedule_definition(timer_id: UUID, schedule_definition: TimerScheduleDefinition) -> None:
+@router.put("/api/v1/timer-tasks/{timer_id}/timer_definition")
+def update_timer_task_timer_definition(timer_id: UUID, schedule_definition: TimerDefinition) -> None:
     return None
 
 
-@router.put("/api/v1/bulk-timers/schedule_definition")
-def batch_update_timer_schedule_definition(schedule_definitions: TimerScheduleDefinition) -> None:
+@router.put("/api/v1/bulk-timers/timer_definition")
+def batch_update_timer_task_timer_definition(schedule_definitions: TimerDefinition) -> None:
     return None
 
 
-@router.get("/api/v1/timers/{timer_id}/details")
-def find_timer(timer_id: UUID) -> Optional[TimerDetails]:
+@router.get("/api/v1/timer-tasks/{timer_id}/details")
+def find_timer_task(timer_id: UUID) -> Optional[TimerTaskDetails]:
     return None
 
 
-@router.get("/api/v1/timers/total")
-def count_timer(timer_ids: List[UUID] = Query([])) -> int:
+@router.get("/api/v1/timer-tasks/total")
+def count_timer_task(timer_ids: List[UUID] = Query([])) -> int:
     return 0
 
 
-@router.get("/api/v1/timers")
-def list_timer(timer_ids: List[UUID] = Query([])) -> List[Timer]:
+@router.get("/api/v1/timer-tasks")
+def list_timer_task(timer_ids: List[UUID] = Query([])) -> List[TimerTask]:
     return []
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from app.interface import timer_routers
+from app.interface import timer_task_routers
 
 app = FastAPI()
-app.include_router(timer_routers.router)
+app.include_router(timer_task_routers.router)
 
 if __name__ == '__main__':
     import uvicorn
